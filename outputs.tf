502973d129137567d37bdd2c3e2c781dfe2d9521
Nick Mathias <nicholas.mathias@peopleshores.com>
2026-03-16 13:02:38 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "ec2_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.spring_boot_app.public_ip
+}
+
+output "s3_bucket_url" {
+  description = "S3 website URL"
+  value       = aws_s3_bucket_website_configuration.frontend.website_endpoint
+}
