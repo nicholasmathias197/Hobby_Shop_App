ab2dbd5b11ce59bf3c5571e5d1507561724d4bcc
nicholasmathias197 <nicholas.mathias@peopleshores.com>
2026-03-16 19:57:11 -0400

    Add React frontend build and S3 static website hosting

--- main.tf
+++ main.tf
@@ -155,3 +155,27 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
     key = "index.html"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.react_frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket     = aws_s3_bucket.react_frontend.id
+  depends_on = [aws_s3_bucket_public_access_block.frontend]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.react_frontend.arn}/*"
+    }]
+  })
+}
