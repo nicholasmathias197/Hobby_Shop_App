4e96e3ebc07d86dc199a1bc352bc5b575c0fb50d
nicholasmathias197 <nicholas.mathias@peopleshores.com>
2026-03-16 20:27:19 -0400

    Fix AMI ID and remove import blocks

--- main.tf
+++ main.tf
@@ -20,31 +20,6 @@ provider "aws" {
 # Get current account ID
 data "aws_caller_identity" "current" {}
 
-import {
-  to = aws_security_group.spring_boot_sg
-  id = "sg-090cf228c30fca1dc"
-}
-
-import {
-  to = aws_iam_role.ec2_role
-  id = "gundam-hobby-shop-ec2-role"
-}
-
-import {
-  to = aws_iam_instance_profile.ec2_profile
-  id = "gundam-hobby-shop-ec2-profile"
-}
-
-import {
-  to = aws_s3_bucket.react_frontend
-  id = "gundam-hobby-shop-frontend-911784620581"
-}
-
-import {
-  to = aws_s3_bucket_website_configuration.frontend
-  id = "gundam-hobby-shop-frontend-911784620581"
-}
-
 # Security Group
 resource "aws_security_group" "spring_boot_sg" {
   name        = "gundam-hobby-shop-sg"
@@ -104,7 +79,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 # EC2 Instance for Spring Boot
 resource "aws_instance" "spring_boot_app" {
-  ami                    = "ami-0a7d80731ae1b2435"
+  ami                    = "ami-0b0b78dcacbab728f"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.spring_boot_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
